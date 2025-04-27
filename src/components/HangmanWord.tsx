e1d42727aa1867d2638207ccecb25b17014e0a4c
Jens CN <jens.sydaker@gmail.com>
2025-04-27 11:09:10 +0200

    added the hangman wordguessing lines

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -4,10 +4,29 @@ type HangmanWordProps = {
   result?: boolean;
 };
 
-const HangmanWord = () => {
+const HangmanWord = ({
+  guessLetters,
+  wordToGuess,
+  result = false,
+}: HangmanWordProps) => {
   return (
-    <div>
-      <h1 className="test text-sky-600">test</h1>
+    <div className="flex gap-2 text-3xl lg:text-6xl font-bold uppercase">
+      {wordToGuess.split("").map((letter, index) => (
+        <span className="border-b-7 border-black rounded-md" key={index}>
+          <span
+            style={{
+              visibility:
+                guessLetters.includes(letter) || result ? "visible" : "hidden",
+              color:
+                !guessLetters.includes(letter) && result
+                  ? "#BE123C"
+                  : "#1C1917",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
     </div>
   );
 };
